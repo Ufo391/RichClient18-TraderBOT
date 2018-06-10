a41cdf086f69af92df49bef578261aa255742c58
Ufo391 <nk-west@hotmail.de>
2018-06-10 15:29:46 +0200

    login.js angelegt

--- Sourcecode/reactjs-server/serverside/src/App.js
+++ Sourcecode/reactjs-server/serverside/src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
 
 class App extends Component {
+
   render() {
-    return (
+    return ( 1
+      /*
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcomeeee to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+     */ 
     );
   }
 }
---
+++ Sourcecode/reactjs-server/serverside/src/components/Login.js
@@ -0,0 +1,19 @@
+class Login extends Component {
+    render() {
+      return (
+        
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcomeeee to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+       
+      );
+    }
+  }
+  
+  export default Login;
