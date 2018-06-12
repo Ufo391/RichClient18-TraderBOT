8d4ad00ec8cd49392f1e2e9fbcd6b34361783a68
Ufo391 <nk-west@hotmail.de>
2018-06-12 16:44:47 +0200

    letzter Stand

--- Sourcecode/reactjs-server/serverside/src/App.js
+++ Sourcecode/reactjs-server/serverside/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Login from './components/Login';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+
 class App extends Component {
 
 
@@ -11,36 +12,16 @@ class App extends Component {
   render() {
     return (    
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcomeeee to React</h1>
-        </header>
-        
-        <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/topics">Topics</Link>
-        </li>
-      </ul>
+          <Container>
+    <Header as='h1'>Hello world!</Header>
 
-      <hr />
-
-      <Route exact path="/" component={Login} />
-      <Route path="/about" component={Login} />
-      <Route path="/topics" component={Login} />
-    </div>
-  </Router>
-
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    <Button
+      content='Discover docs'
+      href='https://react.semantic-ui.com'
+      icon='github'
+      labelPosition='left'
+    />
+</Container>
       </div>
      
     );
