4938ba8b9d6635c346bb806c0b3b44cb025b0346
Ufo391 <nk-west@hotmail.de>
2018-06-12 16:12:02 +0200

    React-Router-Modul eingefügt

--- Sourcecode/reactjs-server/serverside/src/App.js
+++ Sourcecode/reactjs-server/serverside/src/App.js
@@ -2,22 +2,47 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
 
+
+  
   render() {
-    return ( 1
-      /*
+    return (    
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcomeeee to React</h1>
         </header>
+        
+        <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/topics">Topics</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <Route exact path="/" component={Login} />
+      <Route path="/about" component={Login} />
+      <Route path="/topics" component={Login} />
+    </div>
+  </Router>
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-     */ 
+     
     );
   }
 }
