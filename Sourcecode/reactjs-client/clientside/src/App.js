279520c4c07077457c9cdc309a0207b955d305dc
Ufo391 <nk-west@hotmail.de>
2018-07-05 17:42:53 +0200

    Implementierung als Single-Page angefangen

--- Sourcecode/reactjs-client/clientside/src/App.js
+++ Sourcecode/reactjs-client/clientside/src/App.js
@@ -5,17 +5,73 @@ import Home from './components/Home';
 import Register from './components/Register';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-class App extends Component {  
+class App extends Component {
+
+  // Konstruktor
+  constructor(props) {
+    super(props);
+    this.state = {
+
+      viewState: -1,
+      VIEWS_ENUM: { LOGIN_STATE: 0, REGISTER_STATE: 1, HOME_STATE: 2 }
+
+    };
+
+    this.state.viewState = this.state.VIEWS_ENUM.LOGIN_STATE;
+
+  }
+
   render() {
-    return (   
-      <Router> 
-        <div className="App">
-          <Route exact path='/' component={Login}/>
-          <Route path='/home' component={Home}/>
-          <Route path='/register' component={Register}/>
-        </div> 
-      </Router>    
-    );
+
+/*
+    switch (this.state.viewState) {
+      case this.state.VIEWS_ENUM.HOME:
+        return ({ Home });
+      case this.state.VIEWS_ENUM.LOGIN:
+        return ({ Login });
+      case this.state.VIEWS_ENUM.REGISTER:
+        return ({ Register });
+      default:
+        break;
+    }
+    */
+
+    var x = 5
+    if(x === 0){return (<Eins/>);}
+    else{return (<Zwei/>);}
+    /*
+        return (
+    
+          <Router>
+            <div className="App">
+              <Route exact path='/' component={Login} />
+              <Route path='/home' component={Home} />
+              <Route path='/register' component={Register} />
+            </div>
+          </Router>
+    
+        );
+        */
+  }
+}
+
+class Eins extends React.Component {
+  render() {
+     return (
+        <div>
+           <h1>1</h1>
+        </div>
+     );
+  }
+}
+
+class Zwei extends React.Component {
+  render() {
+     return (
+        <div>
+           <h1>2</h1>
+        </div>
+     );
   }
 }
 
